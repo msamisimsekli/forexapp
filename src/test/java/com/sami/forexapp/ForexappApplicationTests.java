22abb65865c4d2dd7d3d89de34db9d3a875e6a1a
Sami <msami.simsekli@gmail.com>
2022-01-23 15:50:42 +0300

    spring boot init

---
+++ src/test/java/com/sami/forexapp/ForexappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sami.forexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
